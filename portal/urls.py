e2fe44d76bd7a3c0fd43d5bbe5cfa9860a132390
Alfred Ssekagiri <assekagiri@gmail.com>
2019-06-25 12:41:33 +0300

    Update portal urls

--- portal/urls.py
+++ portal/urls.py
@@ -1,7 +1,19 @@
 from django.conf.urls import url
 from django.urls import path, include
+from django.views.generic import TemplateView
+
+from .models import Project
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.view_maps, name='index'),
+    #url(r'^$', TemplateView.as_view(template_name="index.html"), name='home'),
+    #url(r'^$', views.view_maps, name='index'),
+    #path('charts/', views.make_pie_and_barcharts, name='piechart'),
+    path('dashboard/', views.dashboard_charts, name='dashboard'),
 ]
+
+
+#from djgeojson.views import GeoJSONLayerView
+#re_path(r'^(?P<year>[0-9]{4})/(?P<month>0?[1-9]|1[0-2])/',views.index, name='index'),
+#url(r'^data/$', GeoJSONLayerView.as_view(model=Project)),geometry_field="log_lat", properties=("platform","country")), name='data'),
+#url(r'^data/(?P<pk>[0-9]+)$',DetailView.as_view(model=Project), name='index'),
